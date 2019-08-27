4e5979905fceb43fdbac6c341c356ecfa839a635
FORTIN <clement.fortin@capgemini.com>
2019-08-27 14:00:41 +0200

    Ajout de l'installation de l'appli

--- scripts/app.js
+++ scripts/app.js
@@ -235,9 +235,23 @@ function getRaidFromNetwork(coords)
  * @param {string} coords Location object to.
  * @return {Object} The weather forecast, if the request fails, return null.
  */
-function getForecastFromCache(coords) {
-  // CODELAB: Add code to get weather forecast from the caches object.
-
+function getRaidFromCache(coords) {
+	// Get Raid forecast from the caches object.
+	if (!('caches' in window)) {
+	return null;
+	}
+	const url = `${window.location.origin}/forecast/${coords}`;
+	return caches.match(url)
+	.then((response) => {
+	  if (response) {
+		return response.json();
+	  }
+	  return null;
+	})
+	.catch((err) => {
+	  console.error('Error getting data from cache', err);
+	  return null;
+	});
 }
 
 /**
--- scripts/install.js
+++ scripts/install.js
@@ -21,7 +21,8 @@ let deferredInstallPrompt = null;
 const installButton = document.getElementById('butInstall');
 installButton.addEventListener('click', installPWA);
 
-// CODELAB: Add event listener for beforeinstallprompt event
+//Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
 
 /**
@@ -31,8 +32,9 @@ installButton.addEventListener('click', installPWA);
  * @param {Event} evt
  */
 function saveBeforeInstallPromptEvent(evt) {
-  // CODELAB: Add code to save event & show the install button.
-
+	//Save event & show the install button.
+	deferredInstallPrompt = evt;
+	installButton.removeAttribute('hidden');
 }
 
 
@@ -42,13 +44,25 @@ function saveBeforeInstallPromptEvent(evt) {
  * @param {Event} evt
  */
 function installPWA(evt) {
-  // CODELAB: Add code show install prompt & hide the install button.
-
-  // CODELAB: Log user response to prompt.
+	//Show install prompt & hide the install button.
+	deferredInstallPrompt.prompt();
+	// Hide the install button, it can't be called twice.
+	evt.srcElement.setAttribute('hidden', true);
+	//Log user response to prompt.
+	deferredInstallPrompt.userChoice
+	.then((choice) => {
+	  if (choice.outcome === 'accepted') {
+		console.log('User accepted the A2HS prompt', choice);
+	  } else {
+		console.log('User dismissed the A2HS prompt', choice);
+	  }
+	  deferredInstallPrompt = null;
+	});
 
 }
 
-// CODELAB: Add event listener for appinstalled event
+//Event listener for appinstalled event
+window.addEventListener('appinstalled', logAppInstalled);
 
 /**
  * Event handler for appinstalled event.
@@ -57,6 +71,6 @@ function installPWA(evt) {
  * @param {Event} evt
  */
 function logAppInstalled(evt) {
-  // CODELAB: Add code to log the event
-
+	//Code to log the event
+	console.log('Raid App was installed.', evt);
 }
